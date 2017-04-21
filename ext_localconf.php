66e48daef53a5696d734dbfef2036afc597c0db0
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-04-21 13:09:24 +0200

    mkpostman parameters added to chash exlude list

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,11 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 tx_rnbase::load('DMK\Mkpostman\Factory');
+
+
+// add some parameters to chash exclude
+tx_rnbase::load('Tx_Rnbase_Utility_Cache');
+Tx_Rnbase_Utility_Cache::addExcludedParametersForCacheHash(array(
+	'mkpostman[key]',
+	'mkpostman[success]',
+));
