4486bdb8dbfb9282c4902867ec172164c5c5936f
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2018-12-03 16:46:55 +0100

    add direct mail tca fields only if direct_mail is installed

--- Configuration/TCA/tx_mkpostman_subscribers.php
+++ Configuration/TCA/tx_mkpostman_subscribers.php
@@ -6,7 +6,7 @@ return call_user_func(
         $lllFile = 'LLL:EXT:mkpostman/Resources/Private/Language/Tca.xlf:';
         $lllTable = $lllFile . 'tx_mkpostman_subscribers.';
 
-        return array(
+        $tca = array(
             'ctrl' => array(
                 'title' => $lllFile . 'tx_mkpostman_subscribers',
                 'label' => 'email',
@@ -99,49 +99,59 @@ return call_user_func(
                         'userFunc' => 'DMK\\Mkpostman\\Utility\\TcaUtility->getLogsForSubscriber'
                     )
                 ),
-                'module_sys_dmail_category' => array(
-                    'displayCond' => 'EXT:direct_mail:LOADED:TRUE',
-                    'exclude' => 1,
-                    'label' => 'LLL:EXT:direct_mail/Resources/Private/Language/locallang_tca.xlf:module_sys_dmail_group.category',
-                    'config' => array(
-                        'type' => 'select',
-                        'foreign_table' => 'sys_dmail_category',
-                        'foreign_table_where' => 'AND sys_dmail_category.l18n_parent=0' .
-                            ' AND sys_dmail_category.pid IN (###PAGE_TSCONFIG_IDLIST###)' .
-                            ' ORDER BY sys_dmail_category.sorting',
-                        'itemsProcFunc' => 'DirectMailTeam\\DirectMail\\SelectCategories->get_localized_categories',
-                        'itemsProcFunc_config' => array(
-                            'table' => 'sys_dmail_category',
-                            'indexField' => 'uid',
-                        ),
-                        'size' => 5,
-                        'minitems' => 0,
-                        'maxitems' => 60,
-                        'renderMode' => 'checkbox',
-                        'MM' => 'tx_mkpostman_subscribers_dmail_category_mm',
-                    )
-                ),
-                'module_sys_dmail_html' => array(
-                    'displayCond' => 'EXT:direct_mail:LOADED:TRUE',
-                    'exclude' => 1,
-                    'label' => 'LLL:EXT:direct_mail/Resources/Private/Language/locallang_tca.xlf:module_sys_dmail_group.htmlemail',
-                    'config' => array(
-                        'type' => 'check'
-                    )
-                )
             ),
             'types' => array(
                 '0' => array(
                     'showitem' => '
                         --div--;' . $lllFile . 'tx_mkpostman_subscribers.tab.general,
                         disabled, email, gender, first_name, last_name, confirmstring, 
-                        --div--;' . $lllFile . 'tx_mkpostman_subscribers.tab.directmail,
-                        module_sys_dmail_category, module_sys_dmail_html,
                         --div--;' . $lllFile . 'tx_mkpostman_subscribers.tab.log,
                         logs,
                     '
                 )
             )
         );
+
+        // add the direct mail columns, if direct mail is installed
+        if (\tx_rnbase_util_Extensions::isLoaded('direct_mail')) {
+            $dmLllFile = 'LLL:EXT:direct_mail/Resources/Private/Language/locallang_tca.xlf:';
+            $tca['columns']['module_sys_dmail_category'] = array(
+                'displayCond' => 'EXT:direct_mail:LOADED:TRUE',
+                'exclude' => 1,
+                'label' => $dmLllFile . 'module_sys_dmail_group.category',
+                'config' => array(
+                    'type' => 'select',
+                    'foreign_table' => 'sys_dmail_category',
+                    'foreign_table_where' => 'AND sys_dmail_category.l18n_parent=0' .
+                        ' AND sys_dmail_category.pid IN (###PAGE_TSCONFIG_IDLIST###)' .
+                        ' ORDER BY sys_dmail_category.sorting',
+                    'itemsProcFunc' => 'DirectMailTeam\\DirectMail\\SelectCategories->get_localized_categories',
+                    'itemsProcFunc_config' => array(
+                        'table' => 'sys_dmail_category',
+                        'indexField' => 'uid',
+                    ),
+                    'size' => 5,
+                    'minitems' => 0,
+                    'maxitems' => 60,
+                    'renderMode' => 'checkbox',
+                    'MM' => 'tx_mkpostman_subscribers_dmail_category_mm',
+                )
+            );
+            $tca['columns']['module_sys_dmail_html'] = array(
+                'displayCond' => 'EXT:direct_mail:LOADED:TRUE',
+                'exclude' => 1,
+                'label' => $dmLllFile . 'module_sys_dmail_group.htmlemail',
+                'config' => array(
+                    'type' => 'check'
+                )
+            );
+
+            $tca['types']['0']['showitem'] .= '
+                --div--;' . $lllFile . 'tx_mkpostman_subscribers.tab.directmail,
+                module_sys_dmail_category, module_sys_dmail_html,
+            ';
+        }
+
+        return $tca;
     }
 );
